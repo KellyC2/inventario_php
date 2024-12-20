6013d3598657a737876e0a4068bcbfeedb3c7b8e
Kelly Condori <kellycondoricondori@gmail.com>
2024-12-20 13:13:35 -0500

    Código que validad datos del formulario

--- php/main.php
+++ php/main.php
@@ -1,8 +1,28 @@
 <?php
+# Conexión a la base de datos
+function conexion()
+{
+    $config = include('config.php');
+    $dsn = "mysql:host={$config['db_host']};dbname={$config['db_name']}";
+    $pdo = new PDO($dsn, $config['db_user'], $config['db_pass']);
+    return $pdo;
+}
 
-    #Conexion a la base de datos
-    function conexion(){
-        $pdo = new PDO('mysql:host=localhost; dbname=inventario', 'root', 'admin123');
-        return $pdo;
+#Verificar datos
+function Verificar_datos($filtro, $cadenaAVerificar){
+    if(preg_match("/^".$filtro."$/", $cadenaAVerificar)){
+        return false;
+    }else{
+        return true;
     }
+
+}
+
+$nombre="Carlos5256";
+if(Verificar_datos("[a-zA-Z]{6,10}", $nombre)){
+    echo "Los datos no coinciden";
+}
+
+
+
 ?>
