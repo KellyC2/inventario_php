f6dca0f5442c451cf2a3bee36d0e5105741bcc6f
Kelly Condori <kellycondoricondori@gmail.com>
2024-12-20 13:30:13 -0500

    Código que que limpia cadena de texto con la función limpiar_cadena() para evitar inyecciones SQL

--- php/main.php
+++ php/main.php
@@ -18,11 +18,39 @@ function Verificar_datos($filtro, $cadenaAVerificar){
 
 }
 
-$nombre="Carlos5256";
-if(Verificar_datos("[a-zA-Z]{6,10}", $nombre)){
-    echo "Los datos no coinciden";
+#Limpiar cadena de texto
+function limpiar_cadena($cadena){
+    $cadena =trim($cadena);
+    $cadena=stripslashes($cadena);
+    $cadena=str_ireplace("<script>","",$cadena);
+    $cadena = str_ireplace("</script>", "", $cadena);
+    $cadena = str_ireplace("<script src", "", $cadena);
+    $cadena = str_ireplace("<script type=", "", $cadena);
+    $cadena = str_ireplace("SELECT * FROM", "", $cadena);
+    $cadena = str_ireplace("DELETE FROM", "", $cadena);
+    $cadena = str_ireplace("INSERT INTO", "", $cadena);
+    $cadena = str_ireplace("DROP TABLE", "", $cadena);
+    $cadena = str_ireplace("DROP DATABASE", "", $cadena);
+    $cadena = str_ireplace("TRUNCATE TABLE", "", $cadena);
+    $cadena = str_ireplace("SHOW TABLES;", "", $cadena);
+    $cadena = str_ireplace("SHOW DATABASES;", "", $cadena);
+    $cadena = str_ireplace("<?php", "", $cadena);
+    $cadena = str_ireplace("?>", "", $cadena);
+    $cadena = str_ireplace("--", "", $cadena);
+    $cadena = str_ireplace("^", "", $cadena);
+    $cadena = str_ireplace("<", "", $cadena);
+    $cadena = str_ireplace("[", "", $cadena);
+    $cadena = str_ireplace("]", "", $cadena);
+    $cadena = str_ireplace("==", "", $cadena);
+    $cadena = str_ireplace(";", "", $cadena);
+    $cadena = str_ireplace("::", "", $cadena);
+    $cadena = trim($cadena);
+    $cadena = stripslashes($cadena);
+    return $cadena;
 }
 
+$texto="<script> Hola Mundo</script>";
+echo limpiar_cadena($texto);
 
 
 ?>
