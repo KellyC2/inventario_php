19031a52f7067b448837c90c05650947d85393d8
Kelly Condori <kellycondoricondori@gmail.com>
2024-12-20 11:49:14 -0500

    Creamos vistas de home, login y 404

--- inc/navbar.php
+++ inc/navbar.php
@@ -1,7 +1,7 @@
 <nav class="navbar" role="navigation" aria-label="main navigation">
     <div class="navbar-brand">
-        <a class="navbar-item" href="https://bulma.io">
-            <img src="./img/logo.png"  width="65" height="28">
+        <a class="navbar-item" href="index.php?vista=home">
+            <img src="./img/logo.png" width="65" height="28">
         </a>
 
         <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
