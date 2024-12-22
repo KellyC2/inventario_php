d3fb33f83fa3dbeefd8a5e2890e369c62f37b31f
Kelly Condori <kellycondoricondori@gmail.com>
2024-12-22 13:41:35 -0500

    Código para restringir o bloquear acceso al sistema a usuario no logueado

--- index.php
+++ index.php
@@ -15,6 +15,15 @@
         }
 
         if(is_file("./vistas/".$_GET['vista'].".php") && $_GET['vista'] != "login" &&  $_GET['vista'] != "404"){
+            
+            #Cerrar session
+            if( (!isset($_SESSION['id'])|| $_SESSION['id']=="") || (!isset($_SESSION['usuario']) || $_SESSION['usuario'] == "") ){
+                include "./vistas/logout.php";
+                exit();
+            }
+            
+            
+            
             include "./inc/navbar.php";
             include "./vistas/".$_GET['vista'].".php";
             include "./inc/script.php";
