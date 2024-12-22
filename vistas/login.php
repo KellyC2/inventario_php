247b0f35445cbef965c68950afb9b998255098fe
Kelly Condori <kellycondoricondori@gmail.com>
2024-12-22 11:48:54 -0500

    Código para destruir o cerrar session con PHP

--- vistas/login.php
+++ vistas/login.php
@@ -21,11 +21,13 @@
         </p>
 
         <?php
-        if (isset($_POST['login_usuario']) && isset($_POST['login_clave'])) {
-            require_once "./php/main.php";
-            require_once "./php/iniciar_sesion.php";
-        }
+            if (isset($_POST['login_usuario']) && isset($_POST['login_clave'])) {
+                require_once "./php/main.php";
+                require_once "./php/iniciar_sesion.php";
+            }
         ?>
+
+    </form>
 
 
 </div>
